157cb8d7c4ed050c81093a270bf93b2a2ce80d66
Anirudh0506 <68997619+Anirudh0506@users.noreply.github.com>
2020-10-01 00:06:31 +0530

    Update and rename hello.c to int-num.py

---
+++ int-num.py
@@ -0,0 +1,42 @@
+#Interesting number
+Given a number 'n', write a program to check if it is a interesting number. 
+A interesting number is a number which can be expressed as sum of the digits 
+raised to its positional value
+ 
+Input format:
+n
+ 
+Output Format:
+Yes/No
+ 
+Sample Input:
+89
+ 
+Sample Output:
+Yes
+ 
+Explanation:
+ 
+8^1+9^2=8+81=89=n. So print 'Yes'
+
+
+
+Input
+89
+Expected output
+Yes
+-----------
+Input
+80
+Expected output
+No
+----------------
+Input
+134
+Expected output
+No
+-----------------
+Input
+2427
+Expected output
+Yes
